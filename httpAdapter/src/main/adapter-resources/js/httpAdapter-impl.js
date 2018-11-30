9e2c1059c60fc61506b3f0770583aaafd275badd
vivinkrishnan <vivinkrishnan@users.noreply.github.com>
2018-11-30 12:24:26 +0530

    Update httpAdapter-impl.js

--- httpAdapter/src/main/adapter-resources/js/httpAdapter-impl.js
+++ httpAdapter/src/main/adapter-resources/js/httpAdapter-impl.js
@@ -5,31 +5,6 @@
  *  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
  */
 
-/**
- * @param tag: a topic such as MobileFirst_Platform, Bluemix, Cordova.
- * @returns json list of items.
- */
-
-function getFeed(tag) {
-	var input = {
-	    method : 'get',
-	    returnedContentType : 'xml',
-	    path : getPath(tag)
-	};
-
-	return MFP.Server.invokeHttp(input);
-}
-
-/**
- * Helper function to build the URL path.
- */
-function getPath(tag){
-    if(tag === undefined || tag === ''){
-        return 'feed.xml';
-    } else {
-        return 'blog/atom/' + tag + '.xml';
-    }
-}
 
 /**
  * @returns ok
